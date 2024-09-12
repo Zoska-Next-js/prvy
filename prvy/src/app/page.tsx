6ed9368e6d9b1af929eeaf28678d96b8ca00e06b
Patrik <patrik.svikruha6@gmail.com>
2024-09-12 08:40:48 +0200

    Initial commit

---
+++ prvy/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography';
+export default function Home() {
+  return (
+
+    <>
+      <Typography variant="h5" gutterBottom>
+        Dobry den
+      </Typography>
+    </>
+  );
+}
